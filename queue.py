d3cf1082ff7e924dae1ae9039f2b87de43c3358c
Leonardo Ruben Arce Mayhua <larcem@gmail.com>
2024-07-16 00:04:06 -0500

    Este es la cola de reproduccion

---
+++ queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("dequeue from empty queue")
+        return self.queue.pop(0)
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("peek from empty queue")
+        return self.queue[0]
+
+    def size(self):
+        return len(self.queue)
