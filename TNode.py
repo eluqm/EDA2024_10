5e815eb05a33773f53277aa25eaf72740852255d
Leonardo Ruben Arce Mayhua <larcem@gmail.com>
2024-07-17 12:36:01 -0500

    TNode del trie modificado para recibir objetos de canciones

--- TNode.py
+++ TNode.py
@@ -1,33 +1,29 @@
 class TNode:
     def __init__(self):
-        self.children ={}
-        self.frecuency= 0
-        self.isEnd=False
+        self.children = {}
+        self.isEnd = False
+        self.song_object = None  # Aquí almacenaremos el objeto Cancion
 
-    def add_children(self,character,child):
-        self.children[character]=child
-    
+    def add_child(self, character, child):
+        self.children[character] = child
+
     def get_children(self):
         return self.children
-    
-    def increments_fecuency(self):
-        self.frecuency =self.frecuency+1
 
-    def decrement_frecuency( self ):
-        self.frecuency+=1 
-    
-    def get_frecuency(self):
-        return self.frecuency
-    
-    def set_end(self,end):
-        self.isEnd=end
-    
+    def set_song_object(self, song_object):
+        self.song_object = song_object
+
+    def get_song_object(self):
+        return self.song_object
+
+    def set_end(self, end):
+        self.isEnd = end
+
     def is_end(self):
         return self.isEnd
-    
+
     def get_child(self, c):
         return self.children.get(c)
-    
-    def has_child(self,character):
+
+    def has_child(self, character):
         return character in self.children
-    
